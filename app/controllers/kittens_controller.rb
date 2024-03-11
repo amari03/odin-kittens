6fb457c37e013dd107e23a24e4cdb522d52e921e
amari03 <2020151434@ub.edu.bz>
2024-03-11 09:37:10 -0600

    Initial commit

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,49 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten, notice: 'Kitten was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: 'Kitten was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+    redirect_to kittens_url, notice: 'Kitten was successfully destroyed.'
+  end
+
+  private
+    def set_kitten
+      @kitten = Kitten.find(params[:id])
+    end
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :breed)
+    end
+end
